9a709a207c0df24057270b725dc4db7363316164
mjkatgithub <melchior.kannengiesser@gmx.de>
2023-04-15 21:03:11 +0200

    more content

--- pages/gemaelde.vue
+++ pages/gemaelde.vue
@@ -1,6 +1,43 @@
 <template>
   <main>
-    gemaelde
+    <table style="margin-top: 20px;" border="0">
+      <tbody>
+      <tr>
+        <td>
+          <img src="~/assets/img/gemaelde/Ukraine.jpg" width="156" border="0"><br>
+          <div class="schilder">Ukraine<br>2023, Aquarell</div>
+        </td>
+        <td>
+          <img src="~/assets/img/gemaelde/Muehlhausen.jpg" width="156" border="0"><br>
+          <div class="schilder">M&uuml;hlhausen<br>2023, Aquarell</div>
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+          <img src="~/assets/img/gemaelde/Rudi_Dutschke.jpg" width="156" border="0"><br>
+          <div class="schilder">Rudi Dutschke<br>2023, Aquarell</div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <img src="~/assets/img/gemaelde/doppelkapelle.png" height="214" width="156" border="0"><br>
+          <div class="schilder">Doppelkapelle<br>St. Crucis in Landsberg<br>2003, Pastel</div>
+        </td>
+        <td>
+          <img src="~/assets/img/gemaelde/rethimnon.png" height="214" width="203" border="0"><br>
+          <div class="schilder">Rethimnon<br>Kreta<br>2000, Aquarell</div>
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>
+          <img src="~/assets/img/gemaelde/dom.png" height="268" width="199" border="0"><br>
+          <div class="schilder">Dom<br>zu Meißen<br>2002, Pastel</div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </main>
 </template>
 

18591f21ccdfa9cb42eac98a363433543f5e4941
mjkatgithub <melchior.kannengiesser@gmx.de>
2023-04-15 14:34:53 +0200

    add vita

---
+++ pages/vita.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <p>
+      friedemann kannengießer studierte bis
+      1973 kirchenmusik an der musikhochschule lübeck. seit 1976 ist er
+      kirchenmusiker in hamburg eimsbüttel, zunächst an der st. stephanus-
+      kirche und von 1999 bis 2017 an der christuskirche. schon in seiner jugend
+      widmet er sich neben der musik auch als autodidakt der plein&nbsp;air
+      malerei und der abstrakten kunst. unter seinem spitznamen kanne
+      entstanden bereits in den 1960-jahren erste werke. es folgten studien
+      bei famous - artists - school und reliev - studien bei ulrich pistor in
+      ortenberg und gießen. die ersten kleinen austellungen unter dem motto
+      „augenblick” konnte man erst 2000 in der villa im park in hamburg, 2003
+      in der pfarrkirche lüdershagen in vorpommern und 2004 und 2011
+      (trilogien) in der christuskirche hamburg eimsbüttel sehen. seit dem
+      zeitalter des computers entdeckte friedemann kannengießer zunehmend auch
+      die künstlerischen möglichkeiten mit digitalen mitteln. neben seiner
+      engagierten arbeit als kirchenmusiker ist so über jahrzehnte ein
+      fruchtbarer bereich des ausgleichs entstanden.
+    </p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "vita"
+}
+</script>
+
+<style scoped>
+  p {
+    text-align: justify;
+  }
+</style>

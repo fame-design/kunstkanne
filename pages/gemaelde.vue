6cb161d76a9cb1a73c0e657ed41a9aa20069128d
mjkatgithub <melchior.kannengiesser@gmx.de>
2023-04-16 11:09:46 +0200

    make it bigger

--- pages/gemaelde.vue
+++ pages/gemaelde.vue
@@ -1,32 +1,32 @@
 <template>
   <main>
     <div>
-      <img src="~/assets/img/gemaelde/Ukraine.jpg" width="156" border="0"><br>
+      <img src="~/assets/img/gemaelde/Ukraine.jpg"><br>
       <div class="schilder">Ukraine<br>2023, Aquarell</div>
     </div>
 
     <div>
-      <img src="~/assets/img/gemaelde/Muehlhausen.jpg" width="156" border="0"><br>
+      <img src="~/assets/img/gemaelde/Muehlhausen.jpg"><br>
       <div class="schilder">M&uuml;hlhausen<br>2023, Aquarell</div>
     </div>
 
     <div>
-      <img src="~/assets/img/gemaelde/Rudi_Dutschke.jpg" width="156" border="0"><br>
+      <img src="~/assets/img/gemaelde/Rudi_Dutschke.jpg"><br>
       <div class="schilder">Rudi Dutschke<br>2023, Aquarell</div>
     </div>
 
     <div>
-      <img src="~/assets/img/gemaelde/doppelkapelle.png" height="214" width="156" border="0"><br>
+      <img src="~/assets/img/gemaelde/doppelkapelle.png"><br>
       <div class="schilder">Doppelkapelle<br>St. Crucis in Landsberg<br>2003, Pastel</div>
     </div>
 
     <div>
-      <img src="~/assets/img/gemaelde/rethimnon.png" height="214" width="203" border="0"><br>
+      <img src="~/assets/img/gemaelde/rethimnon.png"><br>
       <div class="schilder">Rethimnon<br>Kreta<br>2000, Aquarell</div>
     </div>
 
     <div>
-      <img src="~/assets/img/gemaelde/dom.png" height="268" width="199" border="0"><br>
+      <img src="~/assets/img/gemaelde/dom.png"><br>
       <div class="schilder">Dom<br>zu Meißen<br>2002, Pastel</div>
     </div>
   </main>
@@ -45,4 +45,19 @@ export default {
   main {
     text-align: center;
   }
+
+  img {
+    width: 90vw;
+    max-width: 755px;
+  }
+
+  .schilder {
+    border: 1px solid #95A6BF;
+    padding: 5px;
+    font-size: 0.6em;
+    background-color: #D5E6FF;
+    margin-top: 5px;
+    margin-bottom: 25px;
+  }
+
 </style>

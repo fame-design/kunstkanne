162136c903160418b11e3323bafbf89858dd1bd6
mjkatgithub <melchior.kannengiesser@gmx.de>
2023-04-16 10:31:26 +0200

    schnell, schnell

--- pages/gemaelde.vue
+++ pages/gemaelde.vue
@@ -1,43 +1,34 @@
 <template>
   <main>
-    <table style="margin-top: 20px;" border="0">
-      <tbody>
-      <tr>
-        <td style="border: 1px solid #a0a0a0;">
-          <img src="~/assets/img/gemaelde/Ukraine.jpg" width="156" border="0"><br>
-          <div class="schilder">Ukraine<br>2023, Aquarell</div>
-        </td>
-        <td style="border: 1px solid #a0a0a0;">
-          <img src="~/assets/img/gemaelde/Muehlhausen.jpg" width="156" border="0"><br>
-          <div class="schilder">M&uuml;hlhausen<br>2023, Aquarell</div>
-        </td>
-      </tr>
-      <tr>
-        <td style="border: 1px solid #a0a0a0;"></td>
-        <td style="border: 1px solid #a0a0a0;">
-          <img src="~/assets/img/gemaelde/Rudi_Dutschke.jpg" width="156" border="0"><br>
-          <div class="schilder">Rudi Dutschke<br>2023, Aquarell</div>
-        </td>
-      </tr>
-      <tr>
-        <td style="border: 1px solid #a0a0a0;">
-          <img src="~/assets/img/gemaelde/doppelkapelle.png" height="214" width="156" border="0"><br>
-          <div class="schilder">Doppelkapelle<br>St. Crucis in Landsberg<br>2003, Pastel</div>
-        </td>
-        <td style="border: 1px solid #a0a0a0;">
-          <img src="~/assets/img/gemaelde/rethimnon.png" height="214" width="203" border="0"><br>
-          <div class="schilder">Rethimnon<br>Kreta<br>2000, Aquarell</div>
-        </td>
-      </tr>
-      <tr>
-        <td style="border: 1px solid #a0a0a0;"></td>
-        <td style="border: 1px solid #a0a0a0;">
-          <img src="~/assets/img/gemaelde/dom.png" height="268" width="199" border="0"><br>
-          <div class="schilder">Dom<br>zu Meißen<br>2002, Pastel</div>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+    <div>
+      <img src="~/assets/img/gemaelde/Ukraine.jpg" width="156" border="0"><br>
+      <div class="schilder">Ukraine<br>2023, Aquarell</div>
+    </div>
+
+    <div>
+      <img src="~/assets/img/gemaelde/Muehlhausen.jpg" width="156" border="0"><br>
+      <div class="schilder">M&uuml;hlhausen<br>2023, Aquarell</div>
+    </div>
+
+    <div>
+      <img src="~/assets/img/gemaelde/Rudi_Dutschke.jpg" width="156" border="0"><br>
+      <div class="schilder">Rudi Dutschke<br>2023, Aquarell</div>
+    </div>
+
+    <div>
+      <img src="~/assets/img/gemaelde/doppelkapelle.png" height="214" width="156" border="0"><br>
+      <div class="schilder">Doppelkapelle<br>St. Crucis in Landsberg<br>2003, Pastel</div>
+    </div>
+
+    <div>
+      <img src="~/assets/img/gemaelde/rethimnon.png" height="214" width="203" border="0"><br>
+      <div class="schilder">Rethimnon<br>Kreta<br>2000, Aquarell</div>
+    </div>
+
+    <div>
+      <img src="~/assets/img/gemaelde/dom.png" height="268" width="199" border="0"><br>
+      <div class="schilder">Dom<br>zu Meißen<br>2002, Pastel</div>
+    </div>
   </main>
 </template>
 
